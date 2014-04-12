d3b5f1918e15d727c51931345721de67b5d5baa2
Justin Loew <jloloew@gmail.com>
2014-04-11 19:53:46 -0500

    Initial Commit

---
+++ Stela/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Stela
+//
+//  Created by Justin Loew on 4/11/14.
+//  Copyright (c) 2014 Justin Loew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Stela/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Stela
+//
+//  Created by Justin Loew on 4/11/14.
+//  Copyright (c) 2014 Justin Loew. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
